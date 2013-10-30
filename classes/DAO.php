803e49b93c2ce595ac3773f81650082363c51b7a
Andrey TEreshko <andrew.tereshko@gmail.com>
2013-10-30 19:56:36 +0400

    Fixed cast to sql in case SERIAL with length

--- classes/DAO.php
+++ classes/DAO.php
@@ -295,7 +295,7 @@ abstract class DAO extends \Erum\DAOAbstract
             /* @var $column \Sql\Reflection\Column */
 
             // skip serial
-            if( $column->datatype == 'SERIAL' && null == $model->{ $column->name } ) continue;
+            if( preg_match( '/^SERIAL[0-9]*$/i', $column->datatype ) && null == $model->{ $column->name } ) continue;
 
             $columnNames[] = $column->name;
 
